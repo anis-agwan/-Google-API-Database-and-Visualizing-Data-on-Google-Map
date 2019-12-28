f41632b0329e3280b0870284052a0990f3618097
anis-agwan <59297399+anis-agwan@users.noreply.github.com>
2019-12-29 02:03:14 +0530

    Add files via upload

---
+++ where.js
@@ -0,0 +1,17 @@
+myData = [
+[19.330538,72.8155814, 'Killa Rd, Police Colony, Vasai West, Vasai-Virar, Maharashtra 401201, India'],
+[19.0049,73.0283858, 'Sector 32, Seawoods, Navi Mumbai, Maharashtra 400706, India'],
+[18.9315083,72.8381806, 'Fort, Mumbai, Maharashtra 400001, India'],
+[19.0418945,72.8183683, 'Byramji Jeejeebhoy Road, Bandstand Road, Mount Mary, Bandra West, Mumbai, Maharashtra 400050, India'],
+[19.3100052,72.7929643, 'Dongri, Uttan, Mira Bhayandar, Maharashtra 401106, India'],
+[18.9412367,72.838489, '1, Frere Rd, Mumbai Port Trust, Mazgaon, Mumbai, Maharashtra 400001, India'],
+[19.2961054,72.8883326, 'Ghodbander Village, Ghodbunder, Mira Bhayandar, Maharashtra 401107, India'],
+[19.1322719,72.7947674, 'Near Kileshwar Mahadev Mandir, Kileshwar Road, Madh, Ambu Bet, Madh, Mumbai, Maharashtra 400061, India'],
+[19.0420375,72.8381867, 'Al Hussain Cooperative Housing Society, द माहीम मकरंद CHS, माहिम पश्चिम, माहीम, मुम्बई, महाराष्ट्र 400016, India'],
+[18.965633,72.842703, 'Mazagon Fort, Dockyard, Ekta Nagar, Mazgaon, Mumbai, Maharashtra 400010'],
+[19.0467416,72.8644137, '1, Budhiram Seth Chawl, Sion Railway Colony, Kala Qila, Dharavi, Mumbai, Maharashtra 400017, India'],
+[19.0006679,72.86012649999999, '31, Sewri Ford Road, MPT, Sewri, MPT, Sewri, Mumbai, Maharashtra 400015, India'],
+[19.0465882,72.8676815, 'Rd No 1, Air Force Quarters, Sion East, Sion, Mumbai, Maharashtra 400022, India'],
+[19.0237125,72.8168, 'Worli village near worli, Worli Tip, fort, Mumbai, Maharashtra 400030, India'],
+[19.330538,72.8155814, 'Killa Rd, Police Colony, Vasai West, Vasai-Virar, Maharashtra 401201, India']
+];
